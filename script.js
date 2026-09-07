7b2f893f86252f234bd223c11f0bcbe86d41df4e
Kalyan5256 <dasarikalyan40@gmail.com>
2026-09-07 18:16:45 +0530

    update sheets link

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ successModal.addEventListener('click', function(e) {
 const form = document.getElementById('applicationForm');
 const submitBtn = document.getElementById('submitBtn');
 
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx60bkHH13B89q_nthTabNCKEjSgBI-FOj_VcSF97PdebPwgzO2J3JT-rlcxZvToqyLNA/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyknHqIuKd4rF123MeuxjV8L77ISoxbCDPKZOMBMK5tuv1gw3hNpteZHnkMYJDaxGiX/exec';
 
 form.addEventListener('submit', async function(e) {
     e.preventDefault();
